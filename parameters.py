01ab184291d7daeb0aef4891600db52c796021a3
Bilal Akmal <bilalakmal@hotmail.com>
2020-03-22 17:17:47 +0500

    default pop_size is 8.

--- parameters.py
+++ parameters.py
@@ -17,7 +17,7 @@ class Parameters:
     def __init__(
         self,
         genotype_length: int=8,
-        population_size: int=10,
+        population_size: int=8,
         maximum_generations: int=100,
         mutation_rate: float=0.05,
         crossover_rate: float=0.80
